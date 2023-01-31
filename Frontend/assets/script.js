4d19d549d8a2045df1575cd083c29243b6a313ce
Violaine Barral <violainebarral@yahoo.fr>
2023-01-31 12:54:11 +0100

    filters bug fixed

--- Frontend/assets/script.js
+++ Frontend/assets/script.js
@@ -1,5 +1,7 @@
 const portfolio = document.getElementById('portfolio');
 const gallery = document.querySelector('.gallery');
+const selector = portfolio.insertBefore(document.createElement('div'), gallery);
+selector.classList.add('selector');
 
 let work;
 let image;
@@ -9,14 +11,15 @@ let figures = [];
 const categorySet = new Set();
 let categories;
 
-const selector = portfolio.insertBefore(document.createElement('div'), gallery);
-selector.classList.add('selector');
-
 const defaultFilter = selector.appendChild(document.createElement('button'));
 defaultFilter.innerText = "Tous";
 defaultFilter.classList.add('filter-selected');
 const buttons = document.querySelectorAll("#portfolio .selector");
 const filters = document.getElementsByClassName('filter');
+
+let filterOne;
+let filterTwo;
+let filterThree;
 
 //Variable pour stocker le code HTML nécessaire pour créer un lien "modifier" menant à une modale
 const modify = '<a class="js-modal modify"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M373.1 24.97C401.2-3.147 446.8-3.147 474.9 24.97L487 37.09C515.1 65.21 515.1 110.8 487 138.9L289.8 336.2C281.1 344.8 270.4 351.1 258.6 354.5L158.6 383.1C150.2 385.5 141.2 383.1 135 376.1C128.9 370.8 126.5 361.8 128.9 353.4L157.5 253.4C160.9 241.6 167.2 230.9 175.8 222.2L373.1 24.97zM440.1 58.91C431.6 49.54 416.4 49.54 407 58.91L377.9 88L424 134.1L453.1 104.1C462.5 95.6 462.5 80.4 453.1 71.03L440.1 58.91zM203.7 266.6L186.9 325.1L245.4 308.3C249.4 307.2 252.9 305.1 255.8 302.2L390.1 168L344 121.9L209.8 256.2C206.9 259.1 204.8 262.6 203.7 266.6zM200 64C213.3 64 224 74.75 224 88C224 101.3 213.3 112 200 112H88C65.91 112 48 129.9 48 152V424C48 446.1 65.91 464 88 464H360C382.1 464 400 446.1 400 424V312C400 298.7 410.7 288 424 288C437.3 288 448 298.7 448 312V424C448 472.6 408.6 512 360 512H88C39.4 512 0 472.6 0 424V152C0 103.4 39.4 64 88 64H200z" fill="black"/></svg> modifier</a>';
@@ -473,6 +476,38 @@ function showAll(){
     [...figures].forEach(element => {element.style.display = 'initial'});
 }
 
+function preventFilterBugs(e){
+    if (e.target.dataset.category === "1"){
+        if (e.target.classList.contains('filter-selected')){
+            filterOne = true;
+        } else {
+            filterOne = false;
+        }
+    } else if (e.target.dataset.category === "2"){
+        if (e.target.classList.contains('filter-selected')){
+            filterTwo = true;
+        } else {
+            filterTwo = false;
+        }
+    } else if (e.target.dataset.category === "3"){
+        if (e.target.classList.contains('filter-selected')){
+            filterThree = true;
+        } else {
+            filterThree = false;
+        } 
+    } else if (e.target === defaultFilter){
+        filterOne = false;
+        filterTwo = false;
+        filterThree = false;
+    }
+    if (!filterOne && !filterTwo && !filterThree){
+        showAll();
+    }
+    console.log(filterOne);
+    console.log(filterTwo);
+    console.log(filterThree);
+}
+
 //Fonction pour changer le style des filtres et montrer les projets correspondants selon le filtre activé
 function filterSelected(e){
     if (e.target === defaultFilter && !e.target.classList.contains('filter-selected')){
@@ -487,7 +522,7 @@ function filterSelected(e){
                 element.style.display = 'initial';
             }
         })
-    } else {
+    } else if (e.target !== defaultFilter){
         e.target.classList.toggle('filter-selected');
         [...figures].forEach(element => {
             if (e.target.classList.contains('filter-selected') && element.dataset.category === e.target.dataset.category){
@@ -497,12 +532,7 @@ function filterSelected(e){
             }
         })
     }
-}
-
-function backToDefaultFilter(){
-    if (!buttons[0].classList.contains('filter-selected') && !buttons[1].classList.contains('filter-selected') && !buttons[2].classList.contains('filter-selected') && !buttons[3].classList.contains('filter-selected')){
-        showAll();
-    }
+    preventFilterBugs(e);
 }
 
 //EventListener pour écouter le clic de la souris sur les filtres
@@ -510,7 +540,6 @@ buttons.forEach(item => {
     item.addEventListener('click', function(e){
         e.stopPropagation();
         filterSelected(e);
-        backToDefaultFilter();
     })
 })
 
